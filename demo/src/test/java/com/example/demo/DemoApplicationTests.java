5943e608d71a882da34d5a3be70aa5280575ce90
tranhuanhcmus <tranhuanbs02@gmail.com>
2023-11-08 16:19:30 +0700

    convert to Maven

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,14 +1,12 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 
-@ContextConfiguration
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// You can add test logic here
 	}
 
 }
