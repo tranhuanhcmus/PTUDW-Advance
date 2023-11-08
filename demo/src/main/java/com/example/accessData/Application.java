6d2b47829ca77cee95131ddbf50b1436d2170763
luckaruto <lucmten@gmail.com>
2023-11-09 02:17:12 +0700

    secretkey

--- demo/src/main/java/com/example/accessData/Application.java
+++ demo/src/main/java/com/example/accessData/Application.java
@@ -3,10 +3,13 @@ package com.example.accessData;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 public class Application {
 
+	
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
